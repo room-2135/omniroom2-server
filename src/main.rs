51afc6b2dfcdb54536263a909a3775ad2fe1adc1
Nabos <nabos@glargh.fr>
2023-02-07 13:44:29 +0100

    I hate myself for not commiting this when I should have, now everything is mixed up

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
-#[macro_use] extern crate rocket;
-use rocket::{State, Shutdown};
-use rocket::http::{Cookie, Status};
+#[macro_use]
+extern crate rocket;
+use rocket::fairing::{Fairing, Info, Kind};
 use rocket::fs::{relative, FileServer};
-use rocket::response::stream::{EventStream, Event};
-use rocket::serde::{Serialize, Deserialize, json::Json, uuid::Uuid};
-use rocket::tokio::sync::broadcast::{channel, Sender, error::RecvError};
+use rocket::http::Header;
+use rocket::http::{Cookie, Status};
+use rocket::response::stream::{Event, EventStream};
+use rocket::serde::{json::Json, uuid::Uuid, Deserialize, Serialize};
 use rocket::tokio::select;
+use rocket::tokio::sync::broadcast::{channel, error::RecvError, Sender};
+use rocket::Response;
+use rocket::{Shutdown, State};
 
 use rocket::request::{FromRequest, Outcome, Request};
 
@@ -17,13 +21,8 @@ enum Payload {
     CameraDiscovery,
     CameraPing,
     CallInit,
-    SDP {
-        description: String
-    },
-    ICE {
-        index: u32,
-        candidate: String
-    }
+    SDP { description: String },
+    ICE { index: u32, candidate: String },
 }
 
 // Incoming Messages
@@ -31,21 +30,21 @@ enum Payload {
 #[serde(crate = "rocket::serde")]
 struct IncomingMessage {
     pub recipient: Option<String>,
-    pub payload: Payload
+    pub payload: Payload,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct Message {
     pub sender: String,
     pub recipient: Option<String>,
-    pub payload: Payload
+    pub payload: Payload,
 }
 
 #[derive(Clone, Serialize)]
 #[serde(crate = "rocket::serde")]
 struct OutgoingMessage {
     pub sender: String,
-    pub payload: Payload
+    pub payload: Payload,
 }
 
 #[get("/events")]
@@ -82,15 +81,19 @@ fn queue_message(message: IncomingMessage, queue: &State<Sender<Message>>, user:
     match queue.send(Message {
         sender: user.user_id,
         recipient: message.recipient,
-        payload: message.payload
+        payload: message.payload,
     }) {
         Ok(_) => Status::Ok,
-        Err(_) => Status::InternalServerError
+        Err(err) => {
+            eprintln!("{:?}", err);
+            Status::InternalServerError
+        }
     }
 }
 
 #[post("/message", data = "<message>")]
 fn message(message: Json<IncomingMessage>, queue: &State<Sender<Message>>, user: User) -> Status {
+    println!("New message: {:?}", message);
     match message.payload {
         Payload::Welcome => {
             return Status::Ok;
@@ -99,36 +102,39 @@ fn message(message: Json<IncomingMessage>, queue: &State<Sender<Message>>, user:
             if message.recipient != None {
                 return Status::BadRequest;
             }
-        },
+        }
         Payload::CameraDiscovery => {
             if message.recipient != None {
                 return Status::BadRequest;
             }
-        },
+        }
         Payload::CameraPing => {
             if message.recipient == None || message.recipient == Some(user.user_id.clone()) {
                 return Status::BadRequest;
             }
-        },
+        }
         Payload::CallInit => {
             if message.recipient == None || message.recipient == Some(user.user_id.clone()) {
                 return Status::BadRequest;
             }
-        },
-        Payload::SDP {..} => {
+        }
+        Payload::SDP { .. } => {
             if message.recipient == None || message.recipient == Some(user.user_id.clone()) {
                 return Status::BadRequest;
             }
-        },
-        Payload::ICE {..} => {
+        }
+        Payload::ICE { .. } => {
             if message.recipient == None || message.recipient == Some(user.user_id.clone()) {
                 return Status::BadRequest;
             }
-        },
+        }
         //Useful in case of version mismatch between cameras, server and clients
         #[allow(unreachable_patterns)]
         _ => {
-            eprintln!("Error: server does not support this payload type: {:?}", message.payload);
+            eprintln!(
+                "Error: server does not support this payload type: {:?}",
+                message.payload
+            );
         }
     }
     queue_message(message.into_inner(), queue, user)
@@ -149,22 +155,44 @@ impl<'r> FromRequest<'r> for User {
             None => {
                 let new_user_id = Uuid::new_v4().to_hyphenated().to_string();
                 let res = new_user_id.clone();
-                request.cookies().add_private(Cookie::new("user_id", new_user_id));
+                request
+                    .cookies()
+                    .add_private(Cookie::new("user_id", new_user_id));
                 println!("======= generating new user_id: {} =======", res);
                 res
             }
         };
-        rocket::request::Outcome::Success(User {
-            user_id
-        })
+        rocket::request::Outcome::Success(User { user_id })
     }
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
+        .attach(CORS)
         .manage(channel::<Message>(1024).0)
         .mount("/", routes![events, message])
         .mount("/", FileServer::from(relative!("static")).rank(1))
 }
 
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, GET, PATCH, OPTIONS",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
